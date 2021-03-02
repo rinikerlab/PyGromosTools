2e1ea031dc30248e20b5fa50d1aa7bc9a4b21f4f
B_Ries <SchroederB@users.noreply.github.com>
2021-03-02 22:12:02 +0100

    Update __init__.py

--- pygromos/tests/in_testfiles/__init__.py
+++ pygromos/tests/in_testfiles/__init__.py
@@ -1,5 +1,7 @@
 import os
 in_test_file_path = os.path.dirname(__file__)
+in_test_file_path = os.path.abspath(in_test_file_path+"/..")
 print("Input Dir: ",in_test_file_path)
 print("Input Dir ABS: ", os.path.abspath(in_test_file_path))
 print("Im here: ", os.getcwd())
+ 
