872c7a24dd6ea6a43fb3b19fe5e324296d31b51e
B_Ries <SchroederB@users.noreply.github.com>
2021-03-02 22:31:16 +0100

    Update __init__.py

--- pygromos/tests/in_testfiles/__init__.py
+++ pygromos/tests/in_testfiles/__init__.py
@@ -1,5 +1,5 @@
 import os
-in_test_file_path = os.path.dirname(__file__)
+in_test_file_path = os.path.dirname("/home/runner/work/PyGromosTools/pygromos/tests/in_testfiles/__init__.py") #__file__)
 
 print("Input Dir: ",in_test_file_path)
 print("Input Dir ABS: ", os.path.abspath(in_test_file_path))
