a1d815f0a44d36204ac393436bb1e87f69ddb246
B_Ries <benjamin-ries@outlook.com>
2021-05-22 00:36:01 +0200

    Update com_top.py

--- pygromos/gromos/pyGromosPP/com_top.py
+++ pygromos/gromos/pyGromosPP/com_top.py
@@ -13,7 +13,13 @@ from typing import List
 from pygromos.files.topology.top import Top
 
 def com_top(top1:Top, top2:Top, topo_multiplier:List[int]=[1,1], solvFrom1:bool=True, paramFrom1:bool=True, verbose:bool=True) -> Top:
-    """[summary]
+    """
+        Python version of the GROMOS++ program to combine two topologies. 
+        All molecules are combined to a single topology and it can be decided where the solvent and parameters ar taken from.
+
+        This mehod can also be called directly via top1.com_top(top2), top1 + top2 or top1+=top2
+
+        Author: Marc Lehner
 
     Parameters
     ----------
