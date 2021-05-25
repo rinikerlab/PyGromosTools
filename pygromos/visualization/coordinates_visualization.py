a89460cc8bc9a3c8ecea8f3190ceee9e4a755bee
bschroed <m-bsGETH1111>
2021-05-25 21:44:06 +0200

    nasty fixing of: posres force block

--- pygromos/visualization/coordinates_visualization.py
+++ pygromos/visualization/coordinates_visualization.py
@@ -3,16 +3,49 @@
 """
 from pygromos.files.coord.cnf import Cnf
 from pygromos.files.trajectory.trc import Trc
-
+from pygromos.utils.amino_acids import three_letter_aa_lib, ions
+from copy import deepcopy
 import py3Dmol
 
 def show_cnf(cnf: Cnf):
-    xyz_str = cnf.get_xyz()
-    view=py3Dmol.view(width=400, height=400)
-    view.addModel(xyz_str)
-    view.setStyle({"stick":{}})
-    view.zoomTo()
+    cCNF = deepcopy(cnf)
 
+    view=py3Dmol.view(width=400, height=400)
+
+    solute = [resn[:3] for resn in cnf.residues if(cCNF !="SOLV")]
+    if(len([res for res in solute if(res in three_letter_aa_lib)])>15 or "SOLV" in cnf.residues):
+        solv_cnf = deepcopy(cnf)
+
+        pos = []
+        for atomP in cCNF.POSITION:
+            atomP.atomType = atomP.atomType[:1]
+            if(atomP.resName == "SOLV"):
+                pos.append(atomP)
+        solv_cnf = deepcopy(cnf)
+        solv_cnf.POSITION = pos
+
+        xyz_str = cCNF.get_pdb()
+        xyzd_str = cCNF.get_xyz()
+
+        view.addModel(xyz_str)
+        view.addModel(xyzd_str)
+
+        view.setStyle({'resn': solute}, {"stick": {}})
+
+        if(len([res for res in solute if(res in three_letter_aa_lib)])>15):
+            protein = [res for res in solute if(res in three_letter_aa_lib)]
+            view.setStyle({'resn': protein}, {"cartoon": {}})
+            view.setStyle({'cartoon': {'arrows': True, 'tubes': True, 'style': 'oval'}})
+
+        view.setStyle({'resn': "SOLV"}, {"line": {}})        # Solvent
+        view.setStyle({'resn': ions},  {"sphere": {"color": "lightgreen", "radius":0.7}})        # ions
+
+    else:
+        xyz_str = cCNF.get_xyz()
+        view.addModel(xyz_str)
+        view.setStyle({"model": -1}, {"stick": {}})
+
+    view.zoomTo()
     return view
 
 
