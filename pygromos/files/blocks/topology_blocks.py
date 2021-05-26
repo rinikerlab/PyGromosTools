2f5fb4bab5b43af49d8169aa476fe2b54fdbad27
bschroed <m-bsGETH1111>
2021-05-26 11:57:21 +0200

    update & checking jupyter-nb:  - example_gromos_files  - example_gromos_traj fix  - example_gromos_simulation - fix
     - adding ene_analib param
     - fixing FORCEFIELD, MAKETOP versoin
     - less noisy printing

--- pygromos/files/blocks/topology_blocks.py
+++ pygromos/files/blocks/topology_blocks.py
@@ -488,10 +488,11 @@ class FORCEFIELD(_generic_gromos_block):
             self.NAME = NAME[0].strip()
         else:
             super().__init__(name=self.__class__.__name__, used=True, content=content)
+            self.NAME = "\n".join(content)
 
     def block_to_string(self) -> str:
         result = self.name + self.line_seperator
-        result += self.NAME + self.line_seperator
+        #result += self.NAME + self.line_seperator
         result += "END" + self.line_seperator
         return result
 
@@ -504,6 +505,7 @@ class MAKETOPVERSION(_generic_gromos_block):
             self.VERSION = VERSION[0].strip()
         else:
             super().__init__(name=self.__class__.__name__, used=True, content=content)
+            self.VERSION = "\n".join(content)
 
     def block_to_string(self) -> str:
         result = self.name + self.line_seperator
@@ -609,7 +611,6 @@ class DISTANCERESSPEC(_generic_gromos_block):
         result += "#{:>4} {:>5} {:>5} {:>5} {:>5}    {:>5} {:>5} {:>5} {:>5} {:>5}  {:>10} {:>10} {:>3}\n".format(
             *self.RESTRAINTHEADER)
         for x in self.RESTRAINTS:
-            print(x)
             result += x.to_string()
         result += "END\n"
         return result
@@ -814,7 +815,7 @@ class MPERTATOM(_generic_gromos_block):
                 stateNames = [stateNames]
 
             for stateN in stateNames:
-                print(stateN)
+                #print(stateN)
                 stateID = self.STATEIDENTIFIERS.index(stateN)+1
 
                 for atom in self.STATEATOMS:
@@ -1077,7 +1078,7 @@ class PERTATOMPARAM(_generic_gromos_block):
                 stateNames = [stateNames]
 
             for stateN in stateNames:
-                print(stateN)
+                #print(stateN)
                 stateID = self.STATEIDENTIFIERS.index(stateN)+1
 
                 for atom in self.STATEATOMS:
