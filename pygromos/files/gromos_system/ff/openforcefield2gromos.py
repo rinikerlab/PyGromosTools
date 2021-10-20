381748539c333cf5c65298b4c63218afe7caa022
mlehner@ethz.ch <mlehner@student.ethz.ch>
2021-10-20 14:52:22 +0200

    removed factor 2 for harmonic  again

--- pygromos/files/gromos_system/ff/openforcefield2gromos.py
+++ pygromos/files/gromos_system/ff/openforcefield2gromos.py
@@ -91,7 +91,7 @@ class openforcefield2gromos():
                 hQ =  not all([self.openFFTop.atom(x).atomic_number != 1 for x in key])
                 atomI = key[0]+1
                 atomJ = key[1]+1
-                k = 2*force.k.value_in_unit(u.kilojoule / (u.mole * u.nanometer ** 2))
+                k = force.k.value_in_unit(u.kilojoule / (u.mole * u.nanometer ** 2))
                 b0 = force.length.value_in_unit(u.nanometer)
                 self.gromosTop.add_new_bond(k=k, b0=b0, atomI=atomI, atomJ=atomJ, includesH=False) #hQ
         if not hasattr(self.gromosTop, "BONDSTRETCHTYPE"):
@@ -109,7 +109,7 @@ class openforcefield2gromos():
                 atomI=key[0]+1
                 atomJ=key[1]+1
                 atomK=key[2]+1
-                k = 2*force.k.value_in_unit(u.kilojoule / (u.mole * u.radian ** 2))
+                k = force.k.value_in_unit(u.kilojoule / (u.mole * u.radian ** 2))
                 kh = force.k.value_in_unit(u.kilojoule / (u.mole * u.degree ** 2))
                 b0 = force.angle.value_in_unit(u.degree)
                 self.gromosTop.add_new_angle(k=k, kh=kh, b0=b0, atomI=atomI, atomJ=atomJ, atomK=atomK, includesH=False) #hQ
