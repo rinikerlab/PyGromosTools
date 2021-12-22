bf472435c418be7e748190798939b8a03e299694
mlehner@ethz.ch <mlehner@student.ethz.ch>
2021-12-22 10:56:50 +0100

    removed init imports in simulations

--- pygromos/simulations/__init__.py
+++ pygromos/simulations/__init__.py
@@ -1,9 +1,9 @@
 #for nice module access
 
-from modules.general_simulation_modules import simulation
-from modules.preset_simulation_modules import emin, md, sd
-from modules.ti_modules import TI_sampling
+#from pygromos.simulations.modules.general_simulation_modules import simulation
+#from pygromos.simulations.modules.preset_simulation_modules import emin, md, sd
+#from pygromos.simulations.modules.ti_modules import TI_sampling
 
-from approaches.hvap_calculation.hvap_calculation import Hvap_calculation
+#from pygromos.simulations.approaches.hvap_calculation.hvap_calculation import Hvap_calculation
 
-from hpc_queuing import submission_systems
+#from pygromos.simulations.hpc_queuing import submission_systems
