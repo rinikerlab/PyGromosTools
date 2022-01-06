9b810f5294934f089761773a860f629abc7bfe54
kpaul <kpaul@student.ethz.ch>
2022-01-06 08:56:46 +0100

    change NSCALE

--- pygromos/simulations/solvation_free_energy_calculation/solvation_free_energy.py
+++ pygromos/simulations/solvation_free_energy_calculation/solvation_free_energy.py
@@ -716,7 +716,7 @@ class Solvation_free_energy_calculation:
 
         # Add PERTURBATION Block
         pert_block = PERTURBATION(NTG=1, NRDGL=0, RLAM=0, DLAMT=0,
-                                  ALPHLJ=0.5, ALPHC=0.5, NLAM=1, NSCALE=0)
+                                  ALPHLJ=0.5, ALPHC=0.5, NLAM=1, NSCALE=1)
         ti_imd.add_block(block=pert_block)
         print("TI imd",ti_imd)
         return ti_imd
@@ -767,7 +767,9 @@ class Solvation_free_energy_calculation:
 
             # get trg info
             trg = Trg(path + "simulation/" + system_name + "_1/" + system_name + "_1.trg.gz")
-
+            # if l == 10:
+            #     print([trg.database.totals[i][2] for i in range(len(trg.database.totals))])
+            #     exit()
             # Get dhdl
             dhdls = [trg.database.totals[i][2] for i in range(len(trg.database.totals))]
 
