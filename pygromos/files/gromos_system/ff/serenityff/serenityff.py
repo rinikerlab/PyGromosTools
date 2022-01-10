0f49734bb7c739aaac5b79dc31b6bcc17924e31a
mlehner@ethz.ch <mlehner@student.ethz.ch>
2022-01-10 10:24:29 +0100

    removed debug print

--- pygromos/files/gromos_system/ff/serenityff/serenityff.py
+++ pygromos/files/gromos_system/ff/serenityff/serenityff.py
@@ -88,12 +88,7 @@ class serenityff():
             if len(matches) >= 1:
                 for match in matches:
                     idx_in_rdkmol = match[idx]
-                    return_dict[idx_in_rdkmol] = [element + str(pattern[1]), pattern[2]]
-                    if element == "C":
-                        print("----------------")
-                        print(idx_in_rdkmol)
-                        print(Chem.MolToSmarts(mol_pattern))
-                        print("----------------")        
+                    return_dict[idx_in_rdkmol] = [element + str(pattern[1]), pattern[2]]     
         return return_dict
                     
                 
