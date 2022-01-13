5cf313b587c1632c472c1a2d422bbf3cb17cfec9
mlehner@ethz.ch <mlehner@student.ethz.ch>
2022-01-13 20:15:07 +0100

    bugfix qmzone

--- pygromos/files/blocks/qmmm_blocks.py
+++ pygromos/files/blocks/qmmm_blocks.py
@@ -14,16 +14,23 @@ class qmzone_field(_generic_field):
         self.C = C
 
     def to_string(self):
-        str_line = "\t" + str(self.name) + "\t\t\t\t\t\t\t\t" + str(self.A) + "\t" + str(self.B) + "\t" + str(self.C) +"\n"
+        str_line = str(self.name) + "\t\t\t\t\t" + str(self.A) + "\t" + str(self.B) + "\t" + str(self.C) +"\n"
         return str_line
 
 class QMZONE(_topology_table_block):
     NBON:int = 1
-    table_header: Iterable[str] = ["IB", "JB", "ICB"]
+    table_header: Iterable[str] = ["name", "A", "B", "C"]
     table_line_type = qmzone_field
 
     def __init__(self, content: Union[Iterable[qmzone_field], str]):
         super().__init__(content=content, FORCEFIELD=None, MAKETOPVERSION=None)
+
+    def block_to_string(self) -> str:
+        result = self.name + "\n"
+        result += super().block_to_string()
+        result += "END\n"
+        return result
+
 class QMUNIT(_topology_block):
     def __init__(self, content:(str or dict or None or __class__)):
         super().__init__(FORCEFIELD=None, MAKETOPVERSION=None, content=content)
