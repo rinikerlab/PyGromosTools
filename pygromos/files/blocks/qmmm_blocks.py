21cd5a55b5fff46748561e2a7edddad3e5cfaebf
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2022-01-14 11:21:25 +0100

    Added more documentation for qmmm specification file

--- pygromos/files/blocks/qmmm_blocks.py
+++ pygromos/files/blocks/qmmm_blocks.py
@@ -7,19 +7,24 @@ TITLE: TITLE = TITLE
 
 
 class qmzone_field(_generic_field):
-    def __init__(self, name: str, A: int, B: int, C: int):
-        self.name = name
-        self.A = A
-        self.B = B
-        self.C = C
+    def __init__(self, QMEN: str, QMEI: int, QMEZ: int, QMEB: int):
+        # Write comment line
+        # QMEN (QM element name), indicates the atom identifier for this position
+        # QMEI (QM element iterator), specifies an iterator over the atom positions
+        # QMEZ (QM element Z), specifies the nuclear charge Z of the atom position
+        # QMEB (QM element bond), specifies whether a bond can be broken or not (== 0).
+        self.QMEN = QMEN
+        self.QMEI = QMEI
+        self.QMEZ = QMEZ
+        self.QMEB = QMEB
 
     def to_string(self):
-        str_line = str(self.name) + "\t\t\t\t\t" + str(self.A) + "\t" + str(self.B) + "\t" + str(self.C) +"\n"
+        str_line = str(self.QMEN) + "\t\t\t\t\t" + str(self.QMEI) + "\t" + str(self.QMEZ) + "\t" + str(self.QMEB) +"\n"
         return str_line
 
 class QMZONE(_topology_table_block):
     NBON:int = 1
-    table_header: Iterable[str] = ["name", "A", "B", "C"]
+    table_header: Iterable[str] = ["QMEN", "QMEI", "QMEZ", "QMEB"]
     table_line_type = qmzone_field
 
     def __init__(self, content: Union[Iterable[qmzone_field], str]):
