c465484431f0190eb60ee50d7ef523dc5d0d9741
mlehner@ethz.ch <mlehner@student.ethz.ch>
2022-01-17 17:11:11 +0100

    test fix

--- pygromos/tests/test_submission/test_hpc_queuing_submission_systems.py
+++ pygromos/tests/test_submission/test_hpc_queuing_submission_systems.py
@@ -17,7 +17,7 @@ class test_queuing_system(unittest.TestCase):
 
     def test_submit(self):
         sub_job = Submission_job(jobName="test_job", command="echo \" WUHAHAHA\"")
-        self.SubmissionSystem.submit_to_queue(sub_job=sub_job)
+        self.submit_to_queue(sub_job=sub_job)
 
     def test_submit_jobAarray_to_queue(self):
 
@@ -25,26 +25,26 @@ class test_queuing_system(unittest.TestCase):
                                 command="echo \" WUHAHAHA\"",
                                 start_job=1,
                                 end_job=1)
-        self.SubmissionSystem.submit_jobAarray_to_queue(sub_job=sub_job)
+        self.submit_jobAarray_to_queue(sub_job=sub_job)
 
     def test_submit_jobAarray_to_queue1_10(self):
         sub_job2 = Submission_job(jobName="test_job", 
                                 command="echo \" WUHAHAHA\"",
                                 start_job=1,
                                 end_job=10)
-        self.SubmissionSystem.submit_jobAarray_to_queue(sub_job=sub_job2)
+        self.submit_jobAarray_to_queue(sub_job=sub_job2)
 
     def test_search_queue_for_jobname(self):
         if self.file_class == _SubmissionSystem:
             return
         search_job = "Test"
-        self.SubmissionSystem.search_queue_for_jobname(job_name=search_job)
+        self.search_queue_for_jobname(job_name=search_job)
 
     def test_get_jobs_from_queue(self):
         if self.file_class == _SubmissionSystem:
             return
         get_jobs_with = "Test"
-        self.SubmissionSystem.get_jobs_from_queue(job_text=get_jobs_with)
+        self.get_jobs_from_queue(job_text=get_jobs_with)
 
 class test_DUMMY(test_queuing_system):
     file_class = DUMMY
