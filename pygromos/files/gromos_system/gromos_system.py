88d63f076892611d0bf31bb67464532de38351cd
mlehner@ethz.ch <mlehner@student.ethz.ch>
2022-02-21 11:08:38 +0100

    check for None

--- pygromos/files/gromos_system/gromos_system.py
+++ pygromos/files/gromos_system/gromos_system.py
@@ -220,18 +220,19 @@ class Gromos_System():
         if in_cnf_path is None and type(self.mol) == Chem.rdchem.Mol and self.mol.GetNumAtoms() >= 1:
             self.cnf = Cnf(in_value=self.mol)
             #TODO: fix ugly workaround for cnf from rdkit with GROMOS FFs
-            if (self.Forcefield.name == "2016H66" or self.Forcefield.name == "54A7") and os.path.exists(self._gromosPP_bin_dir):
-                try:
-                    from pygromos.files.blocks.coord_blocks import atomP
-                    new_pos = [atomP(xp=atom.xp, yp=atom.yp, zp=atom.zp, 
-                                    resID=atom.resID, atomType=atom.atomType+str(i+1), atomID=atom.atomID,
-                                    resName=self.Forcefield.mol_name) for i, atom in enumerate(self.cnf.POSITION)]
-                    self.cnf.POSITION = new_pos
-                    self.cnf.write_pdb(self.work_folder+"/tmp.pdb")
-                    self.pdb2gromos(self.work_folder+"/tmp.pdb")
-                    self.add_hydrogens()
-                except:
-                    raise Warning("Could not convert cnf from rdkit to gromos, will use rdkit cnf")
+            if (self.Forcefield.name == "2016H66" or self.Forcefield.name == "54A7"):
+                if self._gromosPP_bin_dir is not None and os.path.exists(self._gromosPP_bin_dir):
+                    try:
+                        from pygromos.files.blocks.coord_blocks import atomP
+                        new_pos = [atomP(xp=atom.xp, yp=atom.yp, zp=atom.zp, 
+                                        resID=atom.resID, atomType=atom.atomType+str(i+1), atomID=atom.atomID,
+                                        resName=self.Forcefield.mol_name) for i, atom in enumerate(self.cnf.POSITION)]
+                        self.cnf.POSITION = new_pos
+                        self.cnf.write_pdb(self.work_folder+"/tmp.pdb")
+                        self.pdb2gromos(self.work_folder+"/tmp.pdb")
+                        self.add_hydrogens()
+                    except:
+                        raise Warning("Could not convert cnf from rdkit to gromos, will use rdkit cnf")
             
         
         # decide if the imd should be adapted (force groups etc.)
