1bb29f10f1042ae0f6a0f53e74513daa3d6458af
schroederb <benjamin-ries@outlook.com>
2022-02-23 15:34:40 +0100

    more docu :)

--- pygromos/gromos/pyGromosPP/generate_FETopologyApproach.py
+++ pygromos/gromos/pyGromosPP/generate_FETopologyApproach.py
@@ -1,102 +1,90 @@
-def generate_dual_topology_approach(cnfA, cnfB, topA, topB, eds:bool=False):
-    ##Atom Mapping
-    atom_mappingAB, smart = find_atom_mapping(cnfA=cnfA, cnfB=cnfB)
+"""_summary_
+
+"""
     
-    ##Coordinates
-    cnfA, cnfB = align_cnfs_with_MCS(cnfA=cnfA, cnfB=cnfB, atom_mappingAB=atom_mappingAB)
-    cnf_comb = copy.deepcopy(cnfA)
-    #cnf_comb += cnfB # needs to be implemented
-    
-    for pos in cnfB.POSITION:
-        cnf_comb.POSITION.append(pos)
-    
-    cnf_comb.supress_atomPosition_singulrarities()
+from typing import List, Tuple
+
+from pygromos.files.coord.cnf import Cnf
+from pygromos.files.top.top import Top
+from pygromos.files.top.ptp import Ptp
+from pygromos.files.top.disres import Disres
 
 
-    ##Top
-    nAtoms_top1 = len(top1.SOLUTEATOM)
-    top_comb = copy.deepcopy(top1)
-    top_comb += top2
+
+def generate_dual_topology_approach(cnfs:List[Cnf], tops:List[Top], eds:bool=False, generate_distance_restraints:bool=True)->Tuple[Cnf, Top, Ptp, Disres]:
+    """
+        This function generates a Gromos System, according to the dual topology paradigm. 
+        In this process a coordinate, topology and pertubation file will be generated. 
+        Additionally a distance restraint file will be generated with RestraintMaker.
+        
+        The definition of the dual topology follows the description in:
+            RestraintMaker: A Graph-Based Approach to Select Distance Restraints in Free-Energy Calculations with Dual Topology
+            Benjamin Ries$^\text{\dag}$, Salomé Rieder$^\text{\dag}$, Clemens Rhiner, Philippe H. Hünenberger$^\text{*}$ and Sereina Riniker$^\text{*}$ (2022, JCAMD)
+
+    Parameters
+    ----------
+    cnfs : List[Cnf]
+        The cnf files of the single end-states (for protein-ligands FE -> [ligandA.cnf, ligandB.cnf, ...])
+    tops : List[Top]
+        The top files of the single end-states (for protein-ligands FE -> [ligandA.top, ligandB.top, ...])
+    eds : bool, optional
+        If you want do perform a eds simulation set True, as the ptp blocks vary between eds and lambda dependent calculations, by default False
+    generate_distance_restraints : bool, optional
+        If True, the dual topology approach will additionally contain distance restrained following the linked dual topology principal. 
+        These restraints will be automatically attempted by RestraintMaker. (But double check!)
     
-    ### Pertubation
-    ptp_comb = ptp.Pertubation_topology()
-    
-    if(eds):
-        from pygromos.files.blocks.pertubation_blocks import MPERTATOM
-        from pygromos.files.blocks.pertubation_blocks import  atom_eds_pertubation_state, pertubation_eds_state
+    Returns
+    -------
+    Tuple[Cnf, Top, Ptp, Disres]
+        Returns the prepared Alchemical System files.
+    """
+    pass
 
-        tops = [topA, topB]
-        dummyState = pertubation_eds_state(IAC=22, CHARGE=0)
+def generate_hybrid_topology_approach(cnfs:List[Cnf], tops:List[Cnf], atomMapping:List=None, eds:bool=False)->Tuple[Cnf, Top, Ptp]:
+    """_summary_
 
-        numStates=len(tops)
-        IND = 1
-        atom_states = []
-        for top_ind, top in enumerate(tops):
-            for atom in top.SOLUTEATOM:        
-                states = {}
-                for ctop in range(ntops):
-                    if(ctop==top_ind):
-                        states.update({ctop+1:pertubation_eds_state(IAC=atom.IAC, CHARGE=atom.CG)})
-                    else:
-                        states.update({ctop+1:dummyState})
+        The definition of the hybrid topology follows the description in:
+            RestraintMaker: A Graph-Based Approach to Select Distance Restraints in Free-Energy Calculations with Dual Topology
+            Benjamin Ries$^\text{\dag}$, Salomé Rieder$^\text{\dag}$, Clemens Rhiner, Philippe H. Hünenberger$^\text{*}$ and Sereina Riniker$^\text{*}$ (2022, JCAMD)
 
-                atom_ptp = atom_eds_pertubation_state(NR=IND, NAME=atom.PANM, STATES=states)
-                atom_states.append(atom_ptp)
-                IND+=1
+    Parameters
+    ----------
+    cnfs : List[Cnf]
+        The cnf files of the single end-states (for protein-ligands FE -> [ligandA.cnf, ligandB.cnf, ...])
+    tops : List[Top]
+        The top files of the single end-states (for protein-ligands FE -> [ligandA.top, ligandB.top, ...])
+    atomMapping : List, optional
+        The mapping of the atoms for the different end-states, by default None
+    eds : bool, optional
+        If you want do perform a eds simulation set True, as the ptp blocks vary between eds and lambda dependent calculations, by default False
 
-        ptp_comb.add_block(block=MPERTATOM(NJLA=len(atom_states), NPTB=numStates, STATEATOMS=atom_states))
+    Returns
+    -------
+    Tuple[Cnf, Top, Ptp]
+        Returns the prepared Alchemical System files.
+    """
+    pass
 
-    else:
-        from pygromos.files.blocks.pertubation_blocks import PERTATOMPARAM
-        from pygromos.files.blocks.pertubation_blocks import  atom_lam_pertubation_state, pertubation_lam_state_nonbonded
+def generate_single_topology_approach(cnfs:List[Cnf], tops:List[Top], eds:bool=False)->Tuple[Cnf, Top, Ptp]:
+    """_summary_
 
-        tops = [topA, topB]
-        build_dummyState = lambda m: pertubation_lam_state_nonbonded(IAC=22, CHARGE=0, MASS=m)
+        The definition of the single topology follows the description in:
+            RestraintMaker: A Graph-Based Approach to Select Distance Restraints in Free-Energy Calculations with Dual Topology
+            Benjamin Ries$^\text{\dag}$, Salomé Rieder$^\text{\dag}$, Clemens Rhiner, Philippe H. Hünenberger$^\text{*}$ and Sereina Riniker$^\text{*}$ (2022, JCAMD)
 
-        numStates=len(tops)
-        IND = 1
-        atom_states = []
-        for top_ind, top in enumerate(tops):
-            for atom in top.SOLUTEATOM:        
-                states = {}
-                for ctop in range(ntops):
-                    if(ctop==top_ind):
-                        states.update({ctop+1:pertubation_lam_state_nonbonded(IAC=atom.IAC, CHARGE=atom.CG, MASS=atom.MASS)})
-                    else:
-                        states.update({ctop+1:build_dummyState(atom.MASS)})
+    Parameters
+    ----------
+    cnfs : List[Cnf]
+        The cnf files of the single end-states (for protein-ligands FE -> [ligandA.cnf, ligandB.cnf, ...])
+    tops : List[Top]
+        The top files of the single end-states (for protein-ligands FE -> [ligandA.top, ligandB.top, ...])
+    eds : bool, optional
+        If you want do perform a eds simulation set True, as the ptp blocks vary between eds and lambda dependent calculations, by default False
 
-                atom_ptp = atom_lam_pertubation_state(NR=IND, RES=atom.MRES, NAME=atom.PANM, STATES=states,)
-                atom_states.append(atom_ptp)
-                IND+=1
 
-        ptp_comb.add_block(block=PERTATOMPARAM(NJLA=len(atom_states), STATEATOMS=atom_states))
-    
-    return cnf_comb, top_comb, ptp_comb
-
-def generate_hybrid_topology_approach(cnfA, cnfB, topA, topB):
-    ##Atom Mapping
-    atom_mappingAB, smart = find_atom_mapping(cnfA=cnfA, cnfB=cnfB)
-    
-    ##Coordinates
-    cnfA, cnfB = align_cnfs_with_MCS(cnfA=cnfA, cnfB=cnfB, atom_mappingAB=atom_mappingAB)
-    cnf_comb, present_atoms = merge_states(cnfA=cmol1, cnfB=cmol2, atomMatchingAB=atom_mappingAB, dist_tresh=0.0, _doNotChangeAtomType=True, _doUpdateAtomMapping=True) #no distance collapsing
-    
-    ##Top
-    
-    
-    ### Pertubation
-    
-    
-    return cnf_comb
-    #return cnf_comb, top_comb, ptp_comb
-
-def generate_single_topology_approach(cnfA, cnfB, topA, topB):
-    ##Atom Mapping
-    atom_mappingAB, smart = find_atom_mapping(cnfA=cnfA, cnfB=cnfB)
-    
-    ##Coordinates
-    cnfA, cnfB = align_cnfs_with_MCS(cnfA=cnfA, cnfB=cnfB, atom_mappingAB=atom_mappingAB)
-    cnf_comb,present_atoms = merge_states(cnfA=cmol1, cnfB=cmol2, atomMatchingAB=atom_mappingAB, dist_tresh=0.09) #no distance collapsing
-    
-
-    return cnf_comb #, top_comb, ptp_comb
+    Returns
+    -------
+    Tuple[Cnf, Top, Ptp]
+        Returns the prepared Alchemical System files.
+    """
+    pass
