d90c3289534d2329875d4e323a6286a378a58b9f
mlehner@ethz.ch <mlehner@student.ethz.ch>
2022-02-25 18:10:21 +0100

    reformatting with pre-commit

--- pygromos/tests/test_utils/test_bash.py
+++ pygromos/tests/test_utils/test_bash.py
@@ -1,6 +1,9 @@
 from pygromos.utils import bash
 import os
 
+# test for basic bash functionality
+
+
 def test_which():
     # there should be an installation of bash
     cmd = "bash"
@@ -11,6 +14,7 @@ def test_which():
     assert isinstance(returned_path, str)
     assert (expected_path_1 == returned_path) or (expected_path_2 == returned_path)
 
+
 def test_command_exists():
     # there should be an installation of python
     cmd = "python"
@@ -20,12 +24,14 @@ def test_command_exists():
     assert isinstance(expected_command_exists, bool)
     assert expected_command_exists == returned_command_exists
 
+
 def test_path_exists():
     expected_outcome = True
     returned_outcome = bash.path_exists(os.getcwd())
 
     assert isinstance(returned_outcome, bool)
     assert expected_outcome == returned_outcome
+
 
 def test_is_directory():
     expected_outcome = True
@@ -34,6 +40,7 @@ def test_is_directory():
     assert isinstance(returned_outcome, bool)
     assert expected_outcome == returned_outcome
 
+
 def test_is_file():
     expected_outcome = True
     returned_outcome = bash.is_file(__file__)
@@ -41,12 +48,14 @@ def test_is_file():
     assert isinstance(returned_outcome, bool)
     assert expected_outcome == returned_outcome
 
+
 def test_directory_exists():
     expected_outcome = True
     returned_outcome = bash.directory_exists(os.getcwd())
 
     assert isinstance(returned_outcome, bool)
     assert expected_outcome == returned_outcome
+
 
 def test_file_exists():
     expected_outcome = True
