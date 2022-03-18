0f7c97d8565d77f0c75387639095d08ca2d14b74
mlehner@ethz.ch <mlehner@student.ethz.ch>
2022-03-18 15:43:30 +0100

    gomosff

--- pygromos/files/forcefield/gromos/gromosff.py
+++ pygromos/files/forcefield/gromos/gromosff.py
@@ -1,11 +1,16 @@
 from typing import List
-from pygromos.data import topology_templates
-from pygromos.files.forcefield._generic_force_field import _generic_force_field
-from pygromos.files.topology.top import Top
-from pygromos.files.topology.ifp import Ifp
-from pygromos.files.topology.mtb import Mtb
+
+# from pygromos.data import topology_templates
 from pygromos.data.ff.Gromos2016H66 import ifp, mtb, mtb_orga
 from pygromos.data.ff.Gromos54A7 import ifp as ifp_54a7, mtb as mtb_54a7
+
+from pygromos.files.forcefield._generic_force_field import _generic_force_field
+from pygromos.files.topology.top import Top
+from pygromos.files.coord.cnf import Cnf
+from pygromos.files.topology.ifp import Ifp
+from pygromos.files.topology.mtb import Mtb
+
+from pygromos.gromos.gromosPP import GromosPP
 
 
 class GromosFF(_generic_force_field):
@@ -19,57 +24,30 @@ class GromosFF(_generic_force_field):
             self.ifp = Ifp(ifp)
             self.mtb = Mtb(mtb)
             self.mtb_orga = Mtb(mtb_orga)
+            self.in_building_block_lib_path = [mtb, mtb_orga]
+            self.in_parameter_lib_path = [ifp]
         else:
             self.ifp = Ifp(ifp_54a7)
             self.mtb = Mtb(mtb_54a7)
             self.mtb_orga = None
+            self.in_building_block_lib_path = [mtb_54a7]
+            self.in_parameter_lib_path = [ifp_54a7]
 
-    def create_top(self, mol: str, in_top: Top = None) -> Top:
-        if in_top is None:
-            in_top = Top(in_value=topology_templates.topology_template_dir + "/blank_template+spc.top")
-
-        # check if mol is a residue or residue list
-        known_res_names = self.mtb.all_res_names
-        if self.mtb_orga is not None:
-            known_res_names += self.mtb_orga.all_res_names
-        if isinstance(mol, str):
-            if mol in known_res_names:
-                return self.convert_residue_to_top(in_top, mol)
-            else:
-                raise ValueError(
-                    f"{mol} is not a valid residue name"
-                )  # TODO: implement RDKit mol to residue name conversion
-        elif isinstance(mol, list):
-            if all([x in known_res_names for x in mol]):
-                for res in mol:
-                    in_top = self.convert_residue_to_top(in_top, res)
-                return in_top
-            else:
-                raise ValueError(
-                    f"{mol} is not a valid residue name"
-                )  # TODO: implement RDKit mol to residue name conversion
-        return in_top
-
-    def convert_residue_to_top(self, in_top: Top, residue: str) -> Top:
-        # find the mtb block for the resname. and store the block in data
-        data = None
-        if residue in self.mtb.mtb_solutes.keys():
-            data = self.mtb.mtb_solutes[residue]
-        elif residue in self.mtb.mtb_solvents.keys():
-            data = self.mtb.mtb_solvents[residue]
-        elif residue in self.mtb.mtb_ends.keys():
-            data = self.mtb.mtb_ends[residue]
-        elif self.mtb_orga is not None:
-            if residue in self.mtb.mtb_solutes.keys():
-                data = self.mtb.mtb_solutes[residue]
-            elif residue in self.mtb.mtb_solvents.keys():
-                data = self.mtb.mtb_solvents[residue]
-            elif residue in self.mtb.mtb_ends.keys():
-                data = self.mtb.mtb_ends[residue]
+    def create_top(self, mol: str, in_top: Top = None, **kwargs) -> Top:
+        if "residue_list" in kwargs:
+            residue_list = kwargs["residue_list"]
+            work_dir = kwargs["work_dir"]
+            GromosPP.make_top(
+                out_top_path=work_dir + "/temp.top",
+                in_building_block_lib_path=self.in_building_block_lib_path,
+                in_parameter_lib_path=self.in_parameter_lib_path,
+                in_sequence=residue_list,
+            )
         else:
-            raise ValueError(f"{residue} is not a valid residue name")
+            self.creteTopoFromSmiles(mol, in_top, **kwargs)
 
-        # create the topology by going through all fields in data
-        for atom in data.atoms:
-            in_top.add_new_atom()
-        return in_top
+    def create_cnf(self, mol: str, in_cnf: Cnf = None, **kwargs) -> Cnf:
+        return Cnf(None)  # TODO: implement
+
+    def creteTopoFromSmiles(self, mol: str, in_top: Top = None, **kwargs) -> Top:
+        raise NotImplementedError("WIP")
