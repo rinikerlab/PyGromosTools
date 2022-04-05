9e343f1b39870e0cd11d706d064402278c67e7a7
MTLehner <35803803+MTLehner@users.noreply.github.com>
2022-04-05 13:29:19 +0000

    Deploying to gh-pages from @ rinikerlab/PyGromosTools@c6c09515ff760a4d34b84b4b41ae9fc16c709ebd 🚀

--- sphinx_project/build_docs.sh
+++ sphinx_project/build_docs.sh
@@ -9,6 +9,7 @@ cp ../../examples/ex*ipynb ./Examples;
 cp -r ../../examples/developer_examples ./Examples;
 cp -r ../../examples/example_files ./Examples;
 cp ../../examples/t*ipynb ./Tutorials;
+cp -r ../../examples/example_files ./Tutorials;
 
 python conf.py;
 
