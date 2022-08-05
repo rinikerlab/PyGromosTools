cef4ead954b8b7d8903f4c657da3c0e6b45c6377
Felix Pultar <felix.pultar@phys.chem.ethz.ch>
2022-08-05 12:27:21 +0200

    Deleted scrap script
    Signed-off-by: Felix Pultar <felix.pultar@phys.chem.ethz.ch>

--- pygromos/tests/in_testfiles/trc/mdtraj-geometry-tests/dihedral_test.py
+++
@@ -1,6 +0,0 @@
-# compute some dihedrals
-from pygromos.files.trajectory import trc
-
-traj = trc.Trc(traj_path="menthol.trc")
-print(traj.dihedrals(atom_pairs=[[29, 27, 10, 12]]))
-print(traj.dihedrals(atom_pairs=[[4, 10, 12, 15]]))
